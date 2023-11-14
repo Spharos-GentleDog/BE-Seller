9402e7e6bf1d0df59e2f1d6abc35f7072ba0fbb3
daeunk17 <daeunk71@gmail.com>
2023-11-14 17:28:49 +0900

    [fix] Security 설정 제거

--- src/main/java/egenius/Vendor/global/config/security/SecurityConfiguration.java
+++ src/main/java/egenius/Vendor/global/config/security/SecurityConfiguration.java
@@ -22,19 +22,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     // 추후 게이트웨이로 옮길 예정
-    private final JwtAuthenticationFilter jwtTokenProvider;
-    private final AuthenticationProvider authenticationProvider;
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        return request -> {
-            var cors = new org.springframework.web.cors.CorsConfiguration();
-            cors.setAllowedOriginPatterns(List.of("*"));
-            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-            cors.setAllowedHeaders(List.of("*"));
-            return cors;
-        };
-    }
+//    private final JwtAuthenticationFilter jwtTokenProvider;
+//    private final AuthenticationProvider authenticationProvider;
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        return request -> {
+//            var cors = new org.springframework.web.cors.CorsConfiguration();
+//            cors.setAllowedOriginPatterns(List.of("*"));
+//            cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//            cors.setAllowedHeaders(List.of("*"));
+//            return cors;
+//        };
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,21 +45,23 @@ public class SecurityConfiguration {
                         authorizeHttpRequests -> authorizeHttpRequests
                                 // 허용 범위
                                 .requestMatchers(
-                                        "/api/v1/**",
-                                        "/swagger-ui/**",
-                                        "/swagger-resources/**",
-                                        "/api-docs/**")
+//                                        "/api/v1/**",
+//                                        "/swagger-ui/**",
+//                                        "/swagger-resources/**",
+//                                        "/api-docs/**",
+                                        "**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
+                .formLogin(formLogin -> formLogin.disable())
                 .sessionManagement(
                         sessionManagement -> sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                ) // 세션을 생성하지 않음. JWT 인증이기 때문에 상태가 없는(stateless) 세션 정책을 사용
-                .authenticationProvider(authenticationProvider) // 커스텀 인증을 사용
-                // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 전에 추가
-                .addFilterBefore(jwtTokenProvider, UsernamePasswordAuthenticationFilter.class);
+                ); // 세션을 생성하지 않음. JWT 인증이기 때문에 상태가 없는(stateless) 세션 정책을 사용
+//                .authenticationProvider(authenticationProvider) // 커스텀 인증을 사용
+//                // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 전에 추가
+//                .addFilterBefore(jwtTokenProvider, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
